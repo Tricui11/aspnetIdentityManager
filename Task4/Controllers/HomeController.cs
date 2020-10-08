a8bf230cc30dd0572703040acfd1f6c6b904510a
Kari Trust <karitrust1@gmail.com>
2020-10-09 00:57:34 +0300

    Almost done

--- Task4/Controllers/HomeController.cs
+++ Task4/Controllers/HomeController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Task4.Data;
 using Task4.Models;
 
 namespace Task4.Controllers
@@ -25,23 +27,50 @@ namespace Task4.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(_userManager.Users.ToList());
         }
+
+        [HttpPost]
+        public IActionResult Delete(string[] selectedIDs)
+        {
+
+            // сохранение первичных настроек в файл "StartupSettings.json"
+            using (StreamWriter file = System.IO.File.CreateText("666.json"))
+            {
+                Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                serializer.Serialize(file, selectedIDs);
+            }
+            /*
+
+            foreach (var Id in userIDs)
+            {
+  //              Customer obj = db.Customers.Find(customerID);
+  //              db.Customers.Remove(obj);
+
+                User temUser = await _userManager.FindByIdAsync(Id);
+                IdentityResult tempMan = await _userManager.UpdateAsync(temUser);
+            }
+            */
+
+            return Json("All the customers deleted successfully!");
+        }
+
 
         public IActionResult UsersManage()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+ //       [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        
-        [Route("users")]
-   //     [Authorize(Roles = "admin")]
+
+        //      [Route("users")]
+        //     [Authorize(Roles = "admin")]
         //       [HttpGet("api/[action]")]
+        //        <li class="nav-item"><a class="nav-link text-dark" asp-area="" asp-controller="Home" asp-action="UsersManage">Users Manage</a></li>
         public IActionResult UserList()
         {
             var users = _userManager.Users;
